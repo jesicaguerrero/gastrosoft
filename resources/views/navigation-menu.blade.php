bcb0904aad9250bef8c2921f120dd0c4759e9763
JhonDavidHernandezCampus <yonif975@gmail.com>
2025-03-12 18:59:18 -0500

    feat: :sparkles: Cambio

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -83,12 +83,14 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+
                                 <button
                                     class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                     <img class="h-8 w-8 rounded-full object-cover"
                                         src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
                                 {{ Auth::user()->profile_photo_url }}
+                                
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button"
