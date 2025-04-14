bb666ac2a8034321c919df0e7b94a0a1f66aac95
jessica <jeguebla00@gmail.com>
2025-04-14 07:10:25 -0500

    agregue la informacion del local

--- resources/views/informacion.blade.php
+++ resources/views/informacion.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <h3 class="text-lg font-bold">Página de Información</h3>
-                <p>Esta es la página de información de la aplicación.</p>
             </div>
         </div>
     </div>
